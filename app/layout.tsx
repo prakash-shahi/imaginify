d4118337905e0ea55f6b9d6200b7605e1c36ea66
codingnepal <codingnepal2020@gmail.com>
2024-06-17 18:32:06 +0545

    fix page.tsx error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Layout from "./(root)/layout";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
